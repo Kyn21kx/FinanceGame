20390ee763f78821b8ae455847c628f8da1abbf9
Kyn21kx <nefesmeralda@gmail.com>
2025-11-11 10:48:01 -0600

    More component UI

--- scripts/utils/node_component_adapter.gd
+++ scripts/utils/node_component_adapter.gd
@@ -10,6 +10,15 @@ var registered_components := [
 	Components.Throwable,
 	Components.Thrower
 ]
+# TODO: make into a map
+var registered_components_name := [
+	Components.Dash.get_type_name(),
+	Components.Movement.get_type_name(),
+	Components.Controller.get_type_name(),
+	Components.Player.get_type_name(),
+	Components.Throwable.get_type_name(),
+	Components.Thrower.get_type_name()
+]
 
 var editor_interface : EditorInterface = null
 var current_object : Object = null
@@ -17,12 +26,20 @@ var current_container: Control = null
 var component_header_scene = preload("res://addons/component_creator/ui_components/component_header.tscn")
 var current_header: ComponentEditor = null
 var current_dropdown: OptionButton = null
+var global_metadata: Dictionary[Node, Dictionary] = {}
 
 func _can_handle(object: Object) -> bool:
 	return object is MeshInstance3D
 
 
 func safe_add_child_to(parent: Node, instance: Node):
+	if (parent != null):
+		print("Null parent")
+		return
+	if (instance != null):
+		print("Null child")
+		return
+
 	if (instance.get_parent() != null):
 		instance.reparent(parent)
 		return
@@ -39,14 +56,23 @@ func populate_dropdown(dropdown: OptionButton):
 
 
 func render_component_properties(sample_instance):
+	var comp_data: Dictionary = self.global_metadata.get_or_add(self.current_object, {})
+
+	var fields : Dictionary = comp_data.get_or_add(sample_instance.get_type_name(), {})
+	if (fields.is_empty()):
+		comp_data[sample_instance.get_type_name()] = fields
+	
 	var props: Array = sample_instance.get_property_list()
 	for prop in props:
 		if prop.name in ["script", "RefCounted", "Built-in script"]:
 			continue
 		var static_props: Dictionary = sample_instance.get_readonly_props()
 		var read_only : bool = prop.type == TYPE_RID || static_props.get(prop.name)
+		fields[prop.name] = sample_instance.get(prop.name)
+		print("From within loop... field: ", prop.name, "; value: ", fields[prop.name])
 		var control_node : Control = render_property(sample_instance, prop.name, prop.type, read_only)
 		safe_add_child_to(self.current_header, control_node)
+
 
 
 func render_property(comp_instance, prop_name: String, type: int, is_read_only: bool = false) -> Control:
@@ -64,31 +90,33 @@ func render_property(comp_instance, prop_name: String, type: int, is_read_only: 
 	# Create appropriate input control based on type
 	var input_control: Control
 	
+	var current_editable_node := self.current_object
+	var on_change_update := func(p_val):
+		var comp_data : Dictionary = self.global_metadata.get(current_editable_node)
+		var fields : Dictionary = comp_data[comp_instance.get_type_name()]
+		fields[prop_name] = p_val
+		
 	match type:
 		TYPE_BOOL:
-			input_control = UIUtils._create_bool_input(current_value, is_read_only)
+			input_control = UIUtils._create_bool_input(current_value, on_change_update, is_read_only)
 		
 		TYPE_INT:
-			input_control = UIUtils._create_int_input(current_value, is_read_only)
+			input_control = UIUtils._create_int_input(current_value, on_change_update, is_read_only)
 		
 		TYPE_FLOAT:
-			input_control = UIUtils._create_float_input(current_value, is_read_only)
+			input_control = UIUtils._create_float_input(current_value, on_change_update, is_read_only)
 		
 		TYPE_STRING:
-			input_control = UIUtils._create_string_input(current_value, is_read_only)
+			input_control = UIUtils._create_string_input(current_value, on_change_update, is_read_only)
 		
 		TYPE_VECTOR2:
-			input_control = UIUtils._create_vector2_input(current_value, is_read_only)
+			input_control = UIUtils._create_vector2_input(current_value, on_change_update, is_read_only)
 		
 		TYPE_VECTOR3:
-			input_control = UIUtils._create_vector3_input(current_value, is_read_only)
-		
-		TYPE_VECTOR4:
-			input_control = UIUtils._create_vector4_input(current_value, is_read_only)
-		
+			input_control = UIUtils._create_vector3_input(current_value, on_change_update, is_read_only)
 		_:
 			# Fallback for unsupported types
-			input_control = UIUtils._create_string_input(str(current_value), is_read_only)
+			input_control = UIUtils._create_string_input(str(current_value), on_change_update,  is_read_only)
 	
 	safe_add_child_to(hbox, input_control)
 	
@@ -107,6 +135,19 @@ func on_component_selected(item: int):
 	render_component_properties(comp_instance)
 	self.current_dropdown.select(0)
 
+func fetch_component_data() -> void:
+	var comp_data : Dictionary = self.global_metadata
+	if (comp_data.is_empty()):
+		return
+	for comp_dict: Dictionary in comp_data.values():
+		for comp_key in comp_dict:
+			print("Trying to instantiate ", comp_key)
+			# Find index??
+			var index = self.registered_components_name.find(comp_key)
+			var instance = self.registered_components[index].new()
+			print("Instance at runtime? ", instance)
+			render_component_properties(instance)
+
 func option_dropdown() -> Control:
 	var dropdown := OptionButton.new()
 	dropdown.item_selected.connect(on_component_selected)
@@ -119,6 +160,11 @@ func option_dropdown() -> Control:
 
 func _parse_begin(_object: Object) -> void:
 	self.current_object = _object
+	if (self.current_object == null):
+		print("Current object is null!")
+		return
+	var comp_data: Dictionary = self.global_metadata.get_or_add(self.current_object, {})
+	print("Comp data: ", comp_data)
 	var label := Label.new()
 	label.text = "This object will be saved in the ECS simulation!"
 	label.label_settings = LabelSettings.new()
@@ -126,6 +172,7 @@ func _parse_begin(_object: Object) -> void:
 	label.autowrap_mode = TextServer.AUTOWRAP_WORD
 	self.add_custom_control(label)
 	self.current_container = VBoxContainer.new()
+	self.fetch_component_data()
 	self.current_dropdown = self.option_dropdown()
 	self.current_container.add_child(self.current_dropdown)
 	self.add_custom_control(self.current_container)
--- scripts/utils/ui_utils.gd
+++ scripts/utils/ui_utils.gd
@@ -1,79 +1,118 @@
+
+# UIUtils.gd
 class_name UIUtils
 
-static func _create_bool_input(value: bool, _is_read_only: bool = false) -> CheckBox:
+# All factories now require a non-null Callable on_change argument.
+# on_change.call(new_value) will be invoked with the full Variant value.
+
+static func _create_bool_input(value: bool, on_change: Callable, _is_read_only: bool = false) -> CheckBox:
 	var checkbox = CheckBox.new()
-	checkbox.button_pressed = value
+	checkbox.pressed = value
 	checkbox.disabled = _is_read_only
+	checkbox.toggled.connect(func(pressed: bool):
+		on_change.call(pressed)
+	)
 	return checkbox
 
-static func _create_int_input(value: int, _is_read_only: bool = false) -> SpinBox:
+static func _create_int_input(value: int, on_change: Callable, _is_read_only: bool = false) -> SpinBox:
 	var spinbox = SpinBox.new()
 	spinbox.step = 1
 	spinbox.allow_greater = true
 	spinbox.allow_lesser = true
 	spinbox.value = value
 	spinbox.editable = !_is_read_only
+	spinbox.value_changed.connect(func(v):
+		on_change.call(int(v))
+	)
 	return spinbox
 
-static func _create_float_input(value: float, _is_read_only: bool = false) -> SpinBox:
+static func _create_float_input(value: float, on_change: Callable, _is_read_only: bool = false) -> SpinBox:
 	var spinbox = SpinBox.new()
 	spinbox.step = 0.1
 	spinbox.allow_greater = true
 	spinbox.allow_lesser = true
 	spinbox.value = value
 	spinbox.editable = !_is_read_only
+	spinbox.value_changed.connect(func(v):
+		on_change.call(float(v))
+	)
 	return spinbox
 
-static func _create_string_input(value: String, _is_read_only: bool = false) -> LineEdit:
+static func _create_string_input(value: String, on_change: Callable, _is_read_only: bool = false) -> LineEdit:
 	var line_edit = LineEdit.new()
 	line_edit.text = value
 	line_edit.custom_minimum_size.x = 150
 	line_edit.editable = !_is_read_only
+	line_edit.text_changed.connect(func(text: String):
+		on_change.call(text)
+	)
 	return line_edit
 
-static func _create_vector2_input(value: Vector2, _is_read_only: bool = false) -> HBoxContainer:
+static func _create_vector2_input(value: Vector2, on_change: Callable, _is_read_only: bool = false) -> HBoxContainer:
 	var container = HBoxContainer.new()
-	
-	var x_input := _create_float_input(value.x, _is_read_only)
+
+	var x_input := SpinBox.new()
+	x_input.step = 0.1
+	x_input.allow_greater = true
+	x_input.allow_lesser = true
+	x_input.value = value.x
+	x_input.editable = !_is_read_only
 	x_input.custom_minimum_size.x = 80
-	
-	var y_input := _create_float_input(value.y, _is_read_only)
+
+	var y_input := SpinBox.new()
+	y_input.step = 0.1
+	y_input.allow_greater = true
+	y_input.allow_lesser = true
+	y_input.value = value.y
+	y_input.editable = !_is_read_only
 	y_input.custom_minimum_size.x = 80
-	
+
+	var lambda := func _emit_v2(_val: float):
+		on_change.call(Vector2(x_input.value, y_input.value))
+
+	x_input.value_changed.connect(lambda)
+	y_input.value_changed.connect(lambda)
+
 	container.add_child(x_input)
 	container.add_child(y_input)
-	
 	return container
 
-static func _create_vector3_input(value: Vector3, _is_read_only: bool = false) -> HBoxContainer:
+static func _create_vector3_input(value: Vector3, on_change: Callable, _is_read_only: bool = false) -> HBoxContainer:
 	var container = HBoxContainer.new()
-	
-	var x_input := _create_float_input(value.x, _is_read_only)
-	x_input.custom_minimum_size.x = 70
-	
-	var y_input := _create_float_input(value.y, _is_read_only)
-	y_input.custom_minimum_size.x = 70
-	
 
-	var z_input := _create_float_input(value.z, _is_read_only)
+	var x_input := SpinBox.new()
+	x_input.step = 0.1
+	x_input.allow_greater = true
+	x_input.allow_lesser = true
+	x_input.value = value.x
+	x_input.editable = !_is_read_only
+	x_input.custom_minimum_size.x = 70
+
+	var y_input := SpinBox.new()
+	y_input.step = 0.1
+	y_input.allow_greater = true
+	y_input.allow_lesser = true
+	y_input.value = value.y
+	y_input.editable = !_is_read_only
+	y_input.custom_minimum_size.x = 70
+
+	var z_input := SpinBox.new()
+	z_input.step = 0.1
+	z_input.allow_greater = true
+	z_input.allow_lesser = true
+	z_input.value = value.z
+	z_input.editable = !_is_read_only
 	z_input.custom_minimum_size.x = 70
-	
+
+	var lambda := func _emit_v3(_val: float):
+		on_change.call(Vector3(x_input.value, y_input.value, z_input.value))
+
+	x_input.value_changed.connect(lambda)
+	y_input.value_changed.connect(lambda)
+	z_input.value_changed.connect(lambda)
+
 	container.add_child(x_input)
 	container.add_child(y_input)
 	container.add_child(z_input)
-	
 	return container
 
-static func _create_vector4_input(value: Vector4, _is_read_only: bool = false) -> HBoxContainer:
-	var container = HBoxContainer.new()
-	
-	for i in range(4):
-		var input = SpinBox.new()
-		input.step = 0.1
-		input.allow_greater = true
-		input.allow_lesser = true
-		input.value = value[i]
-		input.custom_minimum_size.x = 60
-		container.add_child(input)
-	
-	return container
