f6b22e65c6c2555b4c76d643b8e74d5541a26b37
Wissens <merinogale@gmail.com>
2025-12-09 04:31:38 -0600

    only interact with the closest interactable

--- scripts/systems/interaction_system.gd
+++ scripts/systems/interaction_system.gd
@@ -20,48 +20,70 @@ func _get_interaction_from_input(event : InputEvent, controller : Components.Con
 	return Components.Interaction.None
 
 func _handle_input(event : InputEvent) -> void:
-	self.interactables_query.each(func cb_interactables(interactable_id : RID, interactable_comps: Array):
-		var interactable_comp : Components.Interactable = interactable_comps[0]
-		var interactable_phybod_comp : Components.PhysicsBody = interactable_comps[1]
+	self.interactors_query.each(func cb_interactors(interactor_id : RID, interactor_comps : Array):
+		var interactor_comp : Components.Interactor = interactor_comps[0]
+		var interactor_phybod_comp : Components.PhysicsBody = interactor_comps[1]
+		var interactor_controller_comp : Components.Controller = interactor_comps[2]
 		
-		# enabled / disabled interactable state
-		if interactable_comp.enabled == false:
+		var interaction := self._get_interaction_from_input(event, interactor_controller_comp)
+		
+		# interaction must be valid, Components.Interaction.None interaction is returned if input cannot 
+		# be mapped to any of the current interactor controlls
+		if interaction == Components.Interaction.None:
 			return
 		
-		self.interactors_query.each(func cb_interactors(interactor_id : RID, interactor_comps : Array):
-			var interactor_comp : Components.Interactor = interactor_comps[0]
-			var interactor_phybod_comp : Components.PhysicsBody = interactor_comps[1]
-			var interactor_controller_comp : Components.Controller = interactor_comps[2]
+		var closest_interactable = {
+			"distance_to_interactor": INF,
+			"closest_interactable_id": RID(),
+			"closest_interactable_comp": null
+		}
+		
+		self.interactables_query.each(func cb_interactables(interactable_id : RID, interactable_comps: Array):
+			var interactable_comp : Components.Interactable = interactable_comps[0]
+			var interactable_phybod_comp : Components.PhysicsBody = interactable_comps[1]
+			
+			# enabled / disabled interactable state
+			if interactable_comp.enabled == false:
+				return
 			
 			# interactor must be on interactable range to interact
-			if interactable_phybod_comp.get_transform().origin.distance_to(interactor_phybod_comp.get_transform().origin) > interactable_comp.interaction_range:
+			var distance_to_interactor = interactable_phybod_comp.get_transform().origin.distance_to(interactor_phybod_comp.get_transform().origin)
+			
+			if distance_to_interactor > interactable_comp.interaction_range:
 				return
 			
-			var interaction := self._get_interaction_from_input(event, interactor_controller_comp)
-			
-			# interaction must be valid, Components.Interaction.None interaction is returned if input cannot 
-			# be mapped to any of the current interactor controlls
-			if interaction == Components.Interaction.None:
+			# only consider the closest interactable to interactor
+			if distance_to_interactor > closest_interactable["distance_to_interactor"]:
 				return
 			
-			# cooldown
-			var now : int = Time.get_ticks_msec()
-			
-			if now - interactable_comp.last_interaction < interactable_comp.cooldown:
-				return
-			
-			interactable_comp.last_interaction = now
-			
-			# add interaction event to queue
-			# is the specific interactable system responsability to handle such event and delete it from the queue
-			InteractionEventQueue.register_event(interactable_id, interactor_id, interaction)
+			closest_interactable["distance_to_interactor"] = distance_to_interactor
+			closest_interactable["closest_interactable_id"] = interactable_id
+			closest_interactable["closest_interactable_comp"] = interactable_comp
 			
 			# TODO: (optional) ensure no more than 1 event with the same values exists
 			# TODO: (optional) ensure no more than 1 event per interactable exist 
 			# TODO: lock interaction -> reserve interactable interactions to specific interactor
 			# TODO: handle on_enter / on_exit interactable area events
-			# TODO: handle interactor on multiple interactables interaction range -> make it such that it can only interact with the closest one
 		)
+		
+		var closest_interactable_id : RID = closest_interactable["closest_interactable_id"]
+		var closest_interactable_comp : Components.Interactable = closest_interactable["closest_interactable_comp"]
+		
+		# did any valid interaction took place at all, if not we are done
+		if closest_interactable_comp == null:
+			return
+		
+		# cooldown
+		var now : int = Time.get_ticks_msec()
+		
+		if now - closest_interactable_comp.last_interaction < closest_interactable_comp.cooldown:
+			return
+		
+		closest_interactable_comp.last_interaction = now
+		
+		# add interaction event to queue
+		# is the specific interactable system responsability to handle such event and delete it from the queue
+		InteractionEventQueue.register_event(closest_interactable_id, interactor_id, interaction)
 	)
 	
 func _input(event : InputEvent) -> void:
